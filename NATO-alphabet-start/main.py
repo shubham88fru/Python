0d6de0c29160fe1311e16a6e4366aa29b351cf73
shubham88fru <shubham88fru@yahoo.com>
2024-04-19 21:50:54 -0700

    comprehension

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -25,3 +25,7 @@ for (index, row) in student_data_frame.iterrows():
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+word = input("Enter a word").upper()
+output_list = [phonetic_dict[letter] for letter in word]
