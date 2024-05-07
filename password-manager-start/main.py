839b561fbc81b78a43d62b7b5849904f93dd320f
shubham88fru <shubham88fru@yahoo.com>
2024-05-07 13:18:07 -0700

    read write and update to json file

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
+import json
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -7,17 +10,26 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+    }
+    }
 
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title="oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n Email: {email}\n "
-                                                              f"Password: {password} \n okay?")
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website} - {email} - {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        with open("data.json", "r") as file:
+            data = json.load(file) #read from a json file.
+            data.update(new_data) #update with with new data.
+
+        with open("data.json", "r") as file:
+            json.dump(data, file, indent=4) #write an object to json file.
+
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -29,15 +41,15 @@ logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image)
 canvas.grid(row=0, column=1)
 
-#Labels
+# Labels
 website_label = Label(text="Website")
 website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username")
-email_label.grid(row=2,column=0)
+email_label.grid(row=2, column=0)
 password_label = Label(text="Password")
 password_label.grid(row=2, column=0)
 
-#Entries
+# Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
@@ -47,7 +59,7 @@ email_entry.insert(0, "mail@mail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
-#Buttons
+# Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36)
