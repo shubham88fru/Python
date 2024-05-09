710ee60a878ba4fb49f3c26e025aafcb3a9e5367
shubham88fru <shubham88fru@yahoo.com>
2024-05-09 15:52:33 -0700

    completa

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,8 +1,22 @@
 import smtplib
+import datetime as dt
+import random
+
+now = dt.datetime.now()
+weekday = now.weekday()
 
 my_email = "email@email.com"
 password = "abcd1234()"
-with smtplib.SMTP('smtp.gmail.com', 587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="somemail@email.com", msg="Subject: Hi\n\nHello World!")
+
+if weekday == 0:
+    with open("quotes.txt", "r") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="somemail@email.com",
+                            msg=f"Subject: Monday Motivation\n\n{quote}"
+                            )
