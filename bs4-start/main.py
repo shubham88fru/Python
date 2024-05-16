19da638548ccc0840fef1be1f06b3ccfcc474d92
shubham88fru <shubham88fru@yahoo.com>
2024-05-16 14:45:56 -0700

    web scraping

--- bs4-start/main.py
+++ bs4-start/main.py
@@ -20,3 +20,25 @@ section_heading.get("class")
 
 company_url = soup.select_one(selector="p a")
 all_class_heading = soup.select(".heading")
+
+#############################################
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+print(soup.title)
+articles = soup.find_all(name="a", class_="storylink")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    article_text = article_tag.getText()
+    article_texts.append(article_text)
+    article_link = article_tag.get("href")
+    article_links.append(article_link)
+
+article_upvotes = [int(score.getText().split(" ")[0]) for score in soup.find_all(name="span", class_="score")]
+
+largest_number = max(article_upvotes)
+article_upvotes.index(largest_number)
